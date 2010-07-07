f5a8de3f3e29adc87518846d28d4284db66f05e6
Veraxus <Veraxus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-07 19:21:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/contexture-page-security/trunk@261172 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ group-edit.php
@@ -0,0 +1,137 @@
+<?php
+
+$sqlGetGroupInfo = "SELECT * FROM `{$wpdb->prefix}ps_groups` WHERE `ID` = '{$wpdb->escape($_GET['groupid'])}'";
+$actionmessage = '';
+
+if(!empty($_GET['action'])){
+    switch($_GET['action']){
+        case 'updtgrp':
+            $sqlUpdateGroup = "UPDATE `{$wpdb->prefix}ps_groups` SET group_title = '{$wpdb->escape($_GET['group_name'])}', group_description = '{$wpdb->escape($_GET['group_description'])}' WHERE ID = '{$wpdb->escape($_GET['groupid'])}';";
+            if($wpdb->query($sqlUpdateGroup) === false){
+                $actionmessage = '<div class="error below-h2"><p>An error occurred. Group Details could not be updated.</p></div>';
+            } else {
+                $actionmessage = '<div id="message" class="updated below-h2"><p>Group details have been saved.</p></div>';
+            }
+            break;
+        case 'addusr':
+            //Make sure user exists in db
+            $sqlCheckUserExists = $wpdb->prepare("SELECT * FROM {$wpdb->users} WHERE user_login = '{$wpdb->escape($_GET['add-username'])}'");
+            $UserInfo = $wpdb->query($sqlCheckUserExists);
+            if($UserInfo == 0){
+                $actionmessage = '<div class="error below-h2"><p>User &quot;'.$_GET['add-username'].'&quot; does not exist.</p></div>';
+            } else {
+                //Add user to group
+                $sqlUpdateGroup = "INSERT INTO `{$wpdb->prefix}ps_group_relationships` (grel_group_id, grel_user_id) VALUES ('{$wpdb->escape($_GET['groupid'])}','{$wpdb->get_var($sqlCheckUserExists,0,0)}');";
+                if($wpdb->query($sqlUpdateGroup) === false){
+                    $actionmessage = '<div class="error below-h2"><p>An error occurred. User could not be added to the group.</p></div>';
+                } else {
+                    $actionmessage = '<div id="message" class="updated below-h2"><p>User &quot;'.$_GET['add-username'].'&quot; has been added to the group.</p></div>';
+                }
+            }
+            break;
+        case 'rmvusr':
+            $sqlRemoveUserRel = "DELETE FROM `{$wpdb->prefix}ps_group_relationships` WHERE ID = '{$wpdb->escape($_GET['relid'])}' AND grel_group_id = '{$wpdb->escape($_GET['groupid'])}' AND grel_user_id = '{$wpdb->escape($_GET['usrid'])}';";
+            if($wpdb->query($sqlRemoveUserRel) == 0){
+                $actionmessage = '<div class="error below-h2"><p>An error occurred. User could not be removed from group.</p></div>';
+            } else {
+                $actionmessage = '<div id="message" class="updated below-h2"><p>User &quot;'.$_GET['usrname'].'&quot; was removed from the group.</p></div>';
+            }
+            break;
+        default: break;
+    }
+}
+
+$groupInfo = $wpdb->get_row($sqlGetGroupInfo);
+
+?>
+    <style type="text/css">
+        .group-actions {
+            text-align:right;
+        }
+        .group-actions a {
+            color:red;
+            font-weight:bold;
+        }
+        #grouptable tbody tr:hover td {
+            background:#fffce0;
+        }
+    </style>
+
+    <div class="wrap">
+        <div class="icon32" id="icon-users"><br/></div>
+        <h2>Editing a Group</h2>
+        <?php echo $actionmessage; ?>
+        <?php if (empty($groupInfo->group_title)){ echo '<div id="message" class="error below-h2"><p>A group with that id does not exist.</p></div>'; }else{ ?>
+
+        <form id="editgroup" name="editgroup" class="validate" method="get" action="">
+            <h3>Group Details</h3>
+            <input id="action" name="page" type="hidden" value="ps_groups_edit" />
+            <input id="action" name="action" type="hidden" value="updtgrp" />
+            <input id="groupid" name="groupid" type="hidden" value="<?php echo $wpdb->escape($_GET['groupid']); ?>" />
+            <?php wp_nonce_field('edit-group'); ?>
+            <table class="form-table">
+                <tr class="form-field form-required">
+                    <th scope="row">
+                        <label for="group_name">
+                            Group Name <span class="description">(required)</span>
+                        </label>
+                    </th>
+                    <td>
+                        <input id="group_name" name="group_name" type="text" aria-required="true" class="regular-text" value="<?php echo $groupInfo->group_title; ?>" maxlength="30" />
+                    </td>
+                </tr>
+                <tr class="form-field">
+                    <th scope="row">
+                        <label for="group_description">
+                            Description
+                        </label>
+                    </th>
+                    <td>
+                        <input id="group_description" name="group_description" type="text" aria-required="false" class="regular-text" value="<?php echo $groupInfo->group_description; ?>" maxlength="400" />
+                    </td>
+                </tr>
+            </table>
+            <p class="submit">
+                <input id="savegroupsub" name="savegroupsub" class="button-primary" type="submit" value="Save Changes" onclick="return validateForm(jQuery(this).parents('#addgroup'));"/>
+            </p>
+        </form>
+        <p></p>
+        <form id="addgroupmember" name="addgroupmember" method="get" action="">
+            <h3>Group Members</h3>
+            <div class="tablenav">
+                <input id="action" name="page" type="hidden" value="ps_groups_edit" />
+                <input id="action" name="action" type="hidden" value="addusr" />
+                <input id="groupid" name="groupid" type="hidden" value="<?php echo $wpdb->escape($_GET['groupid']); ?>" />
+                <input id="add-username" name="add-username" class="regular-text" type="text" value="username" onclick="if(jQuery(this).val()=='username'){jQuery(this).val('')}" onblur="if(jQuery(this).val().replace(' ','')==''){jQuery(this).val('username')}" /> <input type="submit" class="button-secondary action" value="Add User" onclick="if(jQuery('#add-username').val().replace(' ','') != '' && jQuery('#add-username').val().replace(' ','') != 'username'){return true;}else{ jQuery('#add-username').css({'border-color':'#CC0000','background-color':'pink'});return false; }" />
+                <?php wp_nonce_field('add-user'); ?>
+            </div>
+            <table id="grouptable" class="widefat fixed" cellspacing="0">
+                <thead>
+                    <tr class="thead">
+                        <th class="username">Username</th>
+                        <th class="name">Name</th>
+                        <th class="email">Email</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr class="thead">
+                        <th class="username">Username</th>
+                        <th class="name">Name</th>
+                        <th class="email">E-mail</th>
+                        <th></th>
+                    </tr>
+                </tfoot>
+                <tbody id="users" class="list:user user-list">
+                    <?php
+                        if(ctx_ps_count_members($_GET['groupid']) == 0){
+                            echo '<td colspan="4">No users have been added to this group.</td>';
+                        } else {
+                            echo ctx_ps_display_member_list($_GET['groupid']);
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </form>
+        <?php } //ENDS : if (empty($groupInfo->group_title)) ?>
+    </div>
---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== Contexture Page Security ===
+Contributors: Matt VanAndel
+Donate link: http://www.contextureintl.com/
+Tags: security, permissions, users, groups, page, post
+Requires at least: 3.0.0
+Tested up to: 3.0.0
+Stable tag: trunk
+
+Allows admins to create user groups and restrict access to sections of the site by user, group, or role.
+
+== Description ==
+
+Contexture Page Security adds some rudimentary "page security" to WordPress, allowing admins to limit or
+restrict access to individual pages or entire sections of their website. To do this, admins create "groups"
+of users that can be added to pages or posts. Only users that are in at least one of those groups are then
+able to access those pages/sections.
+
+For pages, all security permissions are automatically inherited by descendant pages; any groups added to
+descendant pages are treated as ADDITIONAL security requirements for the page and it's descendants (and so
+on). This allows you to create sub-sections with even more security requirements, for additional flexibility.
+
+You can also restrict blog posts. Any users who do not have access to restricted posts (as defined by you)
+will NOT able able to access those posts – nor will excerpts for those posts even appear in the blog lists.
+
+For the most part, Contexture Page Security should be self-explanatory. Visible changes made to the Dashboard are:
+
+1. Two new options are located under Users. Users > Groups and Users > Add Group.
+1. A "Restrict Access" sidebar is added to the "edit page" for both pages and posts (this allows you add security to those pages/posts/sections).
+1. A "Group Membership" overview is added to each user's Edit User page.
+
+Here are some other technical details to keep in mind:
+
+1. If you enable protection for a page/post/section but don't assign any groups to it (or to a protected ancestor), then NOBODY will be able to access that page (except admins).
+1. The sidebar on the "edit page" admin pages are AJAX-loaded, so your changes are made immediately. You do not need to click "Update" to save changes to the page's security.
+
+This plugin is still under development and should NOT be relied on to protect mission critical data. We make
+no warranty about it's effectiveness during real-world use. USE AT YOUR OWN RISK!
+
+== Installation ==
+
+1. Upload `contexture-page-security` folder to your `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. You're finished! Start using Contexture Page Security.
+
+== Screenshots ==
+
+1. screenshot-1.png
+
+== Changelog ==
+
+= 0.7.0 =
+* Basic security features are in place. First releasable version.
+
