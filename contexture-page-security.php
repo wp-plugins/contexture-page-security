d468b55c180af505bf6b7a9b85a731436747ffc2
Veraxus <Veraxus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-07 20:21:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/contexture-page-security/tags/0.7.1@261221 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- contexture-page-security.php
+++ contexture-page-security.php
@@ -4,7 +4,7 @@ Plugin Name: Contexture Page Security
 Plugin URI: http://www.contextureintl.com/open-source-projects/contexture-page-security-for-wordpress/
 Description: Allows admins to create user groups and restrict access to sections of the site by group.
 Version: 0.7.1
-Author: Contexture Intl., Matt Van Andel
+Author: Contexture Intl., Matt VanAndel
 Author URI: http://www.contextureintl.com
 License: GPL2
 */
