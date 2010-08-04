124b98193d5cbfcf7dac1484969273704fa183c7
Veraxus <Veraxus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-04 16:04:20 +0000

    PHP5 requirement is now enforced by activation process.
    git-svn-id: https://plugins.svn.wordpress.org/contexture-page-security/tags/1.0.2@271702 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ group-delete.php
@@ -0,0 +1,58 @@
+<?php
+
+$sqlGetGroupInfo = "SELECT * FROM `{$wpdb->prefix}ps_groups` WHERE `ID` = '{$wpdb->escape($_GET['groupid'])}'";
+$sqlGetGroupPageCount = "SELECT COUNT(*) FROM `{$wpdb->prefix}ps_security` WHERE `sec_access_id` = '{$wpdb->escape($_GET['groupid'])}' AND `sec_access_type` = 'group'";
+
+$groupInfo = $wpdb->get_row($sqlGetGroupInfo);
+$groupPageCount = $wpdb->get_var($sqlGetGroupPageCount);
+
+$actionmessage = '';
+$actionmessage2 = '';
+
+if(!empty($_GET['action']) && !empty($_GET['submit']) && $_GET['action'] == "delete" && $_GET['submit']=="Confirm Deletion"){
+
+    $sqlDeleteGroupRelat = "DELETE FROM `{$wpdb->prefix}ps_group_relationships` WHERE `grel_group_id` = '{$wpdb->escape($_GET['groupid'])}'";
+    $sqlDeleteGroupSecur = "DELETE FROM `{$wpdb->prefix}ps_security` WHERE `sec_access_id` = '{$wpdb->escape($_GET['groupid'])}' AND `sec_access_type` = 'group'";
+    $sqlDeleteGroup = "DELETE FROM `{$wpdb->prefix}ps_groups` WHERE `ID` = '{$wpdb->escape($_GET['groupid'])}'";
+
+    $sqlstatus1 = $wpdb->query($sqlDeleteGroupRelat);
+    $sqlstatus2 = $wpdb->query($sqlDeleteGroupSecur);
+    $sqlstatus3 = $wpdb->query($sqlDeleteGroup);
+
+    if(!$sqlstatus3){
+        $actionmessage = '<div class="error below-h2"><p>An error occurred. The group was not fully deleted.</p></div>';
+    } else {
+        $actionmessage2 = '<div id="message" class="update below-h2"><p><strong>1</strong> group was deleted. <a href="users.php?page=ps_groups">View all groups &gt;&gt;</a></p></div>';
+    }
+}
+?>
+
+    <div class="wrap">
+        <div class="icon32" id="icon-users"><br/></div>
+        <h2>Delete Group</h2>
+        <?php echo $actionmessage; ?>
+        <?php if(!empty($actionmessage2)){ echo $actionmessage2; }else{ ?>
+        <?php 
+            if (empty($groupInfo->group_title)){ //Group doesnt exist error
+                echo '<div id="message" class="error below-h2"><p>A group with that id does not exist. <a href="users.php?page=ps_groups">View all groups &gt;&gt;</a></p></div>';
+            }else if(isset($groupInfo->group_system_id)){ //Group is a system group error (cannot edit)
+                echo '<div id="message" class="error below-h2"><p>System groups cannot be deleted. <a href="users.php?page=ps_groups">View all groups &gt;&gt;</a></p></div>';
+            }else{ 
+        ?>
+        <form id="deletegroup" name="deletegroup" method="get" action="">
+            <input type="hidden" name="page" value="ps_groups_delete"/>
+            <input type="hidden" name="groupid" value="<?php echo $_GET['groupid']; ?>" />
+            <input type="hidden" name="action" value="delete" />
+            <p>You are about to delete the group <strong><?php echo $groupInfo->group_title; ?></strong>.</p>
+            <p>Deleting this group will affect <strong><?php echo ctx_ps_count_members($groupInfo->ID); ?></strong> users and <strong><?php echo $groupPageCount; ?></strong> pages/posts. Are you sure you want to continue?</p>
+            <?php wp_nonce_field('delete-group'); ?>
+            <p class="submit">
+                <input class="button-secondary" type="submit" value="Confirm Deletion" name="submit"/>
+            </p>
+        </form>
+        <?php
+
+            } //ENDS : if (empty($groupInfo->group_title))
+        } //ENDS : if (!empty($actionmessage2))... else ...
+        ?>
+    </div>
