b8e9d57612009f17a08bc81a30d590ef380f1f90
Veraxus <Veraxus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-04 18:30:22 +0000

    A typo in the PHP version check in 1.0.2 broke activation (d'oh). This is now fixed.
    git-svn-id: https://plugins.svn.wordpress.org/contexture-page-security/tags/1.0.3@271796 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ group-new.php
@@ -0,0 +1,34 @@
+
+    <div class="wrap">
+        <div class="icon32" id="icon-users"><br/></div>
+        <h2>Add New Group</h2>
+        <form id="addgroup" name="addgroup" class="validate" method="post" action="?page=ps_groups">
+            <?php wp_nonce_field('add-group'); ?>
+            <input id="action" name="action" type="hidden" value="addgroup" />
+            <table class="form-table">
+                <tr class="form-field form-required">
+                    <th scope="row">
+                        <label for="group_name">
+                            Group Name <span class="description">(required)</span>
+                        </label>
+                    </th>
+                    <td>
+                        <input id="group_name" name="group_name" type="text" aria-required="true" class="regular-text" value="" maxlength="30" />
+                    </td>
+                </tr>
+                <tr class="form-field">
+                    <th scope="row">
+                        <label for="group_description">
+                            Description
+                        </label>
+                    </th>
+                    <td>
+                        <input id="group_description" name="group_description" type="text" aria-required="false" class="regular-text" value="" maxlength="400" />
+                    </td>
+                </tr>
+            </table>
+            <p class="submit">
+                <input id="addgroupsub" name="addgroupsub" class="button-primary" type="submit" value="Add Group" onclick="return validateForm(jQuery(this).parents('#addgroup'));"/>
+            </p>
+        </form>
+    </div>
---
+++ user-edit-groups.php
@@ -0,0 +1,60 @@
+<?php
+global $current_user;
+?>
+    <style type="text/css">
+        #grouptable {
+
+        }
+        #grouptable tbody tr:hover td {
+            background:#fffce0;
+        }
+
+        #grouptable .id {width:30px;}
+        #grouptable .name {width:200px;}
+        #grouptable .description {}
+        #grouptable .user-count {width:60px;}
+
+        #grouptable tbody .name a {font-weight:bold}
+
+    </style>
+    <script type="text/javascript">
+        /**/
+    </script>
+    <div class="wrap">
+        <h3>Group Membership</h3>
+        <table id="grouptable" class="widefat fixed" cellspacing="0">
+            <thead>
+                <tr class="thead">
+                    <th class="id">id</th>
+                    <th class="name">Name</th>
+                    <th class="description">Description</th>
+                    <th class="user-count">Users</th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr class="thead">
+                    <th class="id">id</th>
+                    <th class="name">Name</th>
+                    <th class="description">Description</th>
+                    <th class="user-count">Users</th>
+                </tr>
+            </tfoot>
+            <tbody>
+                <?php
+                    if ( IS_PROFILE_PAGE ) {
+                        if(ctx_ps_count_groups($current_user->ID) == 0){
+                            echo '<td colspan="4">This user has not been added to any static groups. You can enroll users from any <a href="users.php?page=ps_groups">group detail page</a>.</td>';
+                        } else {
+                            echo ctx_ps_display_group_list($current_user->ID);
+                        }
+                    }else{
+                        if(ctx_ps_count_groups($_GET['user_id']) == 0){
+                            echo '<td colspan="4">This user has not been added to any static groups. You can enroll users from any <a href="users.php?page=ps_groups">group detail page</a>.</td>';
+                        } else {
+                            echo ctx_ps_display_group_list($_GET['user_id']);
+                        }
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
