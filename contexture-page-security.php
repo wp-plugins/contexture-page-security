6f06c295240ca47e5603f3a10acea56c40d8aecd
Veraxus <Veraxus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-30 17:08:17 +0000

    Potentially fixed bug that might allow restricted pages content to be partially loaded.
    git-svn-id: https://plugins.svn.wordpress.org/contexture-page-security/trunk@317045 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- contexture-page-security.php
+++ contexture-page-security.php
@@ -459,7 +459,9 @@ function ctx_ps_security_action(){
                     if($dbOpt['ad_msg_usepages']==='true'){ //Have to exempt feed else it interupts feed render
                         //Send user to the new page
                         if(is_numeric($dbOpt['ad_page_anon_id'])){
-                            wp_safe_redirect(get_permalink($dbOpt['ad_page_anon_id']));
+                            $redir_link = get_permalink($dbOpt['ad_page_anon_id']);
+                            wp_safe_redirect($redir_link);
+                            wp_die(sprintf(__('Redirecting to %s'),$redir_link));
                         }else{
                             //Just in case theres a config problem...
                             wp_die($dbOpt['ad_msg_anon'].'<a style="display:block;font-size:0.7em;" href="'.$blogurl.'">&lt;&lt; Go to home page</a>');
@@ -474,7 +476,9 @@ function ctx_ps_security_action(){
                     if($dbOpt['ad_msg_usepages']==='true'){
                         //Send user to the new page
                         if(is_numeric($dbOpt['ad_page_auth_id'])){
+                            $redir_link = get_permalink($dbOpt['ad_page_auth_id']);
                             wp_safe_redirect(get_permalink($dbOpt['ad_page_auth_id']));
+                            wp_die(sprintf(__('Redirecting to %s'),$redir_link));
                         }else{
                             //Just in case theres a config problem...
                             wp_die($dbOpt['ad_msg_auth'].'<a style="display:block;font-size:0.7em;" href="'.$blogurl.'">&lt;&lt; Go to home page</a>');
