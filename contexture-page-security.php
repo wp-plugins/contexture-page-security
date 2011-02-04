01381b8cde71ce1ae5a90d5e3e5b610a07a43d3f
Veraxus <Veraxus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-04 01:23:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/contexture-page-security/branches/1.4.0@340514 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- contexture-page-security.php
+++ contexture-page-security.php
@@ -50,12 +50,12 @@ add_action('admin_init', 'ctx_ps_admin_init');
 add_action('init','ctx_ps_localization');
 
 //Handle Ajax for Edit Page/Post page
-add_action('wp_ajax_ctx_ps_add2page','ctx_ps_ajax_add_group_to_page');
-add_action('wp_ajax_ctx_ps_removefrompage','ctx_ps_ajax_remove_group_from_page');
+add_action('wp_ajax_ctx_ps_add2page','CTXPSAjax::add_group_to_page');
+add_action('wp_ajax_ctx_ps_removefrompage','CTXPSAjax::remove_group_from_page');
 add_action('wp_ajax_ctx_ps_security_update','ctx_ps_ajax_security_update');
 
 //Handle Ajax for Edit User page
-add_action('wp_ajax_ctx_ps_add2user','ctx_ps_ajax_add_group_to_user');
+add_action('wp_ajax_ctx_ps_add2user','CTXPSAjax::add_group_to_user');
 add_action('wp_ajax_ctx_ps_removefromuser','ctx_ps_ajax_remove_group_from_user');
 add_action('wp_ajax_ctx_ps_updatemember','ctx_ps_ajax_update_membership');
 
@@ -63,7 +63,7 @@ add_action('wp_ajax_ctx_ps_updatemember','ctx_ps_ajax_update_membership');
 add_action('wp','ctx_ps_security_action',1);
 
 //Add basic security to dynamically displayed posts (such as on Blog Posts Page, ie: Home) [highest priority]
-add_filter( "the_posts","ctx_ps_security_filter_blog",1);
+add_filter( 'the_posts','ctx_ps_security_filter_blog',1);
 
 //Ensure that menus do not display protected pages (when using default menus only) [highest priority]
 add_filter('get_pages','ctx_ps_security_filter_menu',1);
@@ -87,137 +87,10 @@ add_action('admin_head', 'ctx_ps_append_contextual_help');
 
 
 
-/**
- * Handles ajax requests to add a group to a page. When successful, generates HTML to be used in the "Allowed Groups"
- * section of the "Restrict Page" sidebar. Spits out XML response for AJAX use.
- * @global wpdb $wpdb
- * @global CTXPSC_Tables $ctxpscdb
- */
-function ctx_ps_ajax_add_group_to_page(){
-    global $wpdb, $ctxpscdb;
-
-    //Added in 1.1 - ensures current user is an admin before processing, else returns an error (probably not necessary - but just in case...)
-    if(!current_user_can('edit_pages')){
-        //If user isn't authorized, stop and return error
-        CTXAjax::response(array('code'=>'0','message'=>__('User is unauthorized to make this change.','contexture-page-security')));
-    }
-
-    //Run the query
-    $result = CTXPSC_Queries::add_security($_GET['postid'],$_GET['groupid']);
-
-    if($result!==false){
-        //Start with blank HTML output
-        $OutputHTML = '';
-
-        //Get security info for the specified page and it's parents
-        $securityStatus = ctx_ps_getprotection( $_GET['postid'] );
-
-        //Set $groupcount to 0, because we are about to count the number of groups attached to THIS page...
-        $groupcount = 0;
-        //If there's any security, count the number of groups attached to this page (including inherited groups from ancestors)
-        if(!!$securityStatus) {
-            foreach($securityStatus as $securityGroups){
-                //Increment $groupcount by the number of groups
-                $groupcount = $groupcount+count($securityGroups);
-            }
-        }
-        //Show groups that are already added to this page
-        if($groupcount===0){
-            //Display this if we have no groups (inherited or otherwise)
-            $OutputHTML = '<div><em>'.__('No groups have been added yet.','contexture-page-security').'</em></div>';
-        }else{
-            //Loop through each PAGE (starting with this one and working our way up)
-            foreach($securityStatus as $securityArray->pageid => $securityArray->grouparray){
-                //If the pageid in the array is this page (ie: current page)
-                if($securityArray->pageid == $_GET['postid']){
-                    //Loop through all groups for the CURRENT page
-                    foreach($securityArray->grouparray as $currentGroup->id => $currentGroup->name){
-                        $OutputHTML .= '<div class="ctx-ps-sidebar-group">&bull; <span class="ctx-ps-sidebar-group-title">'.$currentGroup->name.'</span><span class="removegrp" onclick="ctx_ps_remove_group_from_page('.$currentGroup->id.',jQuery(this))">'.__('remove','contexture-page-security').'</span></div>';
-                    }
-                }else{
-                    //Loop through all groups for the ANCESTOR page
-                    foreach($securityArray->grouparray as $currentGroup->id => $currentGroup->name){
-                        $OutputHTML .= '<div class="ctx-ps-sidebar-group inherited">&bull; <span class="ctx-ps-sidebar-group-title">'.$currentGroup->name.'</span><a class="viewgrp" target="_blank" href="'.admin_url().'post.php?post='.$securityArray->pageid.'&action=edit" >'.__('ancestor','contexture-page-security').'</a></div>';
-                    }
-                }
-            }
-        }
-
-        CTXAjax::response(array('code'=>0,'html'=>'<![CDATA['.$OutputHTML.']]>'));
-    }
-}
-
-/**
- * Handles ajax requests to remove a group from a specified page
- */
-function ctx_ps_ajax_remove_group_from_page(){
-    global $wpdb;
 
 
-    //Added in 1.1 - ensures current user is an admin before processing, else returns an error (probably not necessary - but just in case...)
-    if(!current_user_can('manage_options')){
-        //If user isn't authorized
-        CTXAjax::response(array('code'=>'0','message'=>__('Admin user is unauthorized.','contexture-page-security')));
-    }
 
-    if(CTXPSC_Queries::delete_security($_GET['postid'],$_GET['groupid']) !== false){
-        CTXAjax::response(array('code'=>'1','message'=>__('Group removed','contexture-page-security')));
-    }else{
-        CTXAjax::response(array('code'=>'0','message'=>__('Query failed','contexture-page-security')));
-    }
-    CTXAjax::response($response);
-}
 
-/**
- * Handles ajax requests to add a user to a group
- *
- * @global wpdb $wpdb
- */
-function ctx_ps_ajax_add_group_to_user(){
-    global $wpdb;
-    /** Determines if user exists*/
-    $UserInfo = 0;
-    /** Determines if user is already in the group */
-    $UserInGroup = 0;
-    /** SQL for updating the group */
-    $sqlUpdateGroup = 0;
-
-    //Added in 1.1 - ensures current user is an admin before processing, else returns an error (probably not necessary - but just in case...)
-    if(!current_user_can('manage_options')){
-        //If user isn't authorized
-        CTXAjax::response(array('code'=>'0','message'=>__('Admin user is unauthorized.','contexture-page-security')));
-    }
-
-    //Make sure user exists in db
-    $UserInfo = (int)$wpdb->get_var(
-            $wpdb->prepare("SELECT COUNT(*) FROM {$wpdb->users} WHERE {$wpdb->users}.ID = '%s'",
-                    $_GET['user_id']));
-
-    //If this user doesn't exist
-    if($UserInfo === 0){
-        CTXAjax::response(array('code'=>'0','message'=>'User not found'));
-    } else {
-
-        //Make sure user isnt already in the group
-        $UserInGroup = $wpdb->prepare('SELECT COUNT(*) FROM `'.$wpdb->prefix.'ps_group_relationships` WHERE grel_group_id=%s AND grel_user_id=%s',
-                $_GET['groupid'],
-                $_GET['user_id']);
-        if($wpdb->get_var($UserInGroup)>0){
-            CTXAjax::response( array('code'=>'0','message'=>__('Already in group','contexture-page-security')) );
-        }
-
-        //Add user to group
-        $sqlUpdateGroup = $wpdb->prepare("INSERT INTO `{$wpdb->prefix}ps_group_relationships` (grel_group_id, grel_user_id) VALUES ('%s','%s');",
-                $_GET['groupid'],
-                $_GET['user_id']);
-        if($wpdb->query($sqlUpdateGroup) === false){
-            CTXAjax::response( array('code'=>'0','message'=>__('Query failed','contexture-page-security')) );
-        } else {
-            CTXAjax::response( array('code'=>'1','message'=>__('User enrolled in group','contexture-page-security'),'html'=>'<![CDATA['.ctx_ps_display_group_list($_GET['user_id'],'users').']]>') );
-        }
-    }
-
-}
 
 /**
  * Handles ajax requests to update a users membership info
--- controllers/ajax.php
+++ controllers/ajax.php
@@ -27,6 +27,136 @@ class CTXAjax{
 
 class CTXPSAjax extends CTXAjax {
 
+    /**
+     * Handles ajax requests to add a group to a page. When successful, generates HTML to be used in the "Allowed Groups"
+     * section of the "Restrict Page" sidebar. Spits out XML response for AJAX use.
+     * @global wpdb $wpdb
+     * @global CTXPSC_Tables $ctxpscdb
+     */
+    public static function add_group_to_page(){
+        global $wpdb, $ctxpscdb;
+
+        //Added in 1.1 - ensures current user is an admin before processing, else returns an error (probably not necessary - but just in case...)
+        if(!current_user_can('edit_pages')){
+            //If user isn't authorized, stop and return error
+            CTXAjax::response(array('code'=>'0','message'=>__('User is unauthorized to make this change.','contexture-page-security')));
+        }
+
+        //Run the query
+        $result = CTXPSC_Queries::add_security($_GET['postid'],$_GET['groupid']);
+
+        if($result!==false){
+            //Start with blank HTML output
+            $OutputHTML = '';
+
+            //Get security info for the specified page and it's parents
+            $securityStatus = ctx_ps_getprotection( $_GET['postid'] );
+
+            //Set $groupcount to 0, because we are about to count the number of groups attached to THIS page...
+            $groupcount = 0;
+            //If there's any security, count the number of groups attached to this page (including inherited groups from ancestors)
+            if(!!$securityStatus) {
+                foreach($securityStatus as $securityGroups){
+                    //Increment $groupcount by the number of groups
+                    $groupcount = $groupcount+count($securityGroups);
+                }
+            }
+            //Show groups that are already added to this page
+            if($groupcount===0){
+                //Display this if we have no groups (inherited or otherwise)
+                $OutputHTML = '<div><em>'.__('No groups have been added yet.','contexture-page-security').'</em></div>';
+            }else{
+                //Loop through each PAGE (starting with this one and working our way up)
+                foreach($securityStatus as $securityArray->pageid => $securityArray->grouparray){
+                    //If the pageid in the array is this page (ie: current page)
+                    if($securityArray->pageid == $_GET['postid']){
+                        //Loop through all groups for the CURRENT page
+                        foreach($securityArray->grouparray as $currentGroup->id => $currentGroup->name){
+                            $OutputHTML .= '<div class="ctx-ps-sidebar-group">&bull; <span class="ctx-ps-sidebar-group-title">'.$currentGroup->name.'</span><span class="removegrp" onclick="ctx_ps_remove_group_from_page('.$currentGroup->id.',jQuery(this))">'.__('remove','contexture-page-security').'</span></div>';
+                        }
+                    }else{
+                        //Loop through all groups for the ANCESTOR page
+                        foreach($securityArray->grouparray as $currentGroup->id => $currentGroup->name){
+                            $OutputHTML .= '<div class="ctx-ps-sidebar-group inherited">&bull; <span class="ctx-ps-sidebar-group-title">'.$currentGroup->name.'</span><a class="viewgrp" target="_blank" href="'.admin_url().'post.php?post='.$securityArray->pageid.'&action=edit" >'.__('ancestor','contexture-page-security').'</a></div>';
+                        }
+                    }
+                }
+            }
+
+            CTXAjax::response(array('code'=>0,'html'=>'<![CDATA['.$OutputHTML.']]>'));
+        }
+    }
+
+    /**
+     * Handles ajax requests to remove a group from a specified page
+     */
+    function remove_group_from_page(){
+        global $wpdb;
+
+        //Added in 1.1 - ensures current user is an admin before processing, else returns an error (probably not necessary - but just in case...)
+        if(!current_user_can('manage_options')){
+            //If user isn't authorized
+            CTXAjax::response(array('code'=>'0','message'=>__('Admin user is unauthorized.','contexture-page-security')));
+        }
+
+        if(CTXPSC_Queries::delete_security($_GET['postid'],$_GET['groupid']) !== false){
+            CTXAjax::response(array('code'=>'1','message'=>__('Group removed','contexture-page-security')));
+        }else{
+            CTXAjax::response(array('code'=>'0','message'=>__('Query failed','contexture-page-security')));
+        }
+        CTXAjax::response($response);
+    }
+    
+    /**
+     * Handles ajax requests to add a user to a group
+     *
+     * @global wpdb $wpdb
+     */
+    function add_group_to_user(){
+        global $wpdb;
+        /** Determines if user exists*/
+        $UserInfo = 0;
+        /** Determines if user is already in the group */
+        $UserInGroup = 0;
+        /** SQL for updating the group */
+        $sqlUpdateGroup = 0;
+
+        //Added in 1.1 - ensures current user is an admin before processing, else returns an error (probably not necessary - but just in case...)
+        if(!current_user_can('manage_options')){
+            //If user isn't authorized
+            CTXAjax::response(array('code'=>'0','message'=>__('Admin user is unauthorized.','contexture-page-security')));
+        }
+
+        //Make sure user exists in db
+        $UserInfo = (int)$wpdb->get_var(
+                $wpdb->prepare("SELECT COUNT(*) FROM {$wpdb->users} WHERE {$wpdb->users}.ID = '%s'",
+                        $_GET['user_id']));
+
+        //If this user doesn't exist
+        if($UserInfo === 0){
+            CTXAjax::response(array('code'=>'0','message'=>'User not found'));
+        } else {
+
+            //Make sure user isnt already in the group
+            $UserInGroup = $wpdb->prepare('SELECT COUNT(*) FROM `'.$wpdb->prefix.'ps_group_relationships` WHERE grel_group_id=%s AND grel_user_id=%s',
+                    $_GET['groupid'],
+                    $_GET['user_id']);
+            if($wpdb->get_var($UserInGroup)>0){
+                CTXAjax::response( array('code'=>'0','message'=>__('Already in group','contexture-page-security')) );
+            }
+
+            //Add user to group
+            $sqlUpdateGroup = $wpdb->prepare("INSERT INTO `{$wpdb->prefix}ps_group_relationships` (grel_group_id, grel_user_id) VALUES ('%s','%s');",
+                    $_GET['groupid'],
+                    $_GET['user_id']);
+            if($wpdb->query($sqlUpdateGroup) === false){
+                CTXAjax::response( array('code'=>'0','message'=>__('Query failed','contexture-page-security')) );
+            } else {
+                CTXAjax::response( array('code'=>'1','message'=>__('User enrolled in group','contexture-page-security'),'html'=>'<![CDATA['.ctx_ps_display_group_list($_GET['user_id'],'users').']]>') );
+            }
+        }
+
+    }
 
 }
 ?>
